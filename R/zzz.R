580c03a31bfb7381fd93bf9266a6248f6e4dcbff
Sam Gardiner <7697866+gardiners@users.noreply.github.com>
2024-03-20 22:43:11 +1100

    Create a memory cache on startup

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+.onLoad <- function(libname, pkgname) {
+  rlang::run_on_load()
+}
+
+#' Use a memory cache by default
+rlang::on_load({
+  if (is.null(getOption("ozgs.cache"))) {
+    options("ozgs.cache" = cachem::cache_mem())
+  }
+})
