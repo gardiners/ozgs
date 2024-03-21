00424c75c46938c3fbc90dcea2d8d3513c157b7f
Sam Gardiner <7697866+gardiners@users.noreply.github.com>
2024-03-22 01:28:11 +1100

    Proof-of-concept: generated geography getters

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,15 @@
   rlang::run_on_load()
 }
 
-#' Use a memory cache by default
+# Use a memory cache by default
 rlang::on_load({
   if (is.null(getOption("ozgs.cache"))) {
     options("ozgs.cache" = cachem::cache_mem())
   }
 })
+
+#' @export
+aus <- new_getter("AUS")
+
+#' @export
+lga <- new_getter("LGA")
