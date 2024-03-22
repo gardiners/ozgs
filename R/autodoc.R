824286f29310a4b90d967cc1f422192404122c0b
Sam Gardiner <7697866+gardiners@users.noreply.github.com>
2024-03-22 23:28:53 +1100

    Generate documentation from service data

---
+++ R/autodoc.R
@@ -0,0 +1,21 @@
+autodoc <- function(geography_name){
+  descr <- with(services, description[geography == geography_name][1])
+  c(
+    stringr::str_glue("@title {descr}"),
+    stringr::str_glue("@description Fetch ASGS geometries for {descr}."),
+    "@details",
+    "The following combinations of ASGS `edition` and `reference_date` are available:",
+    service_tbl(geography_name),
+    "",
+    "@inheritParams get_geography",
+    "@export"
+  )
+}
+
+service_tbl <- function(geography_name) {
+  services |>
+    dplyr::filter(geography == geography_name) |>
+    dplyr::select(geography, edition, reference_date) |>
+    dplyr::arrange(reference_date) |>
+    knitr::kable(format = "pipe")
+}
