e9d73fb87bae22cdab3b586d8006b4eb4f88ea37
Sam Gardiner <7697866+gardiners@users.noreply.github.com>
2024-03-24 17:42:38 +1100

    Include metadata-generated documentation for geographies, where available

--- R/autodoc.R
+++ R/autodoc.R
@@ -1,18 +1,41 @@
-autodoc <- function(geography_name){
-  descr <- with(services, description[geography == geography_name][1])
+#' Generate documentation for a geography-specific 'getter'
+#'
+#' @param geography_name Character string name of an ASGS geography, as defined
+#'   in the internal dataset `services$geography`.
+#'
+#' @return A character vector of Roxygen2 documentation for the geograpgy.
+#'
+#' @noRd
+rd_autodoc <- function(geography_name){
+  asgs_geography <- latest_metadata(geography_name, "asgs_geography")
+  asgs_description <- latest_metadata(geography_name, "description") |>
+    clean_description()
+
   c(
-    stringr::str_glue("@title {descr}"),
-    stringr::str_glue("@description Fetch ASGS geometries for {descr}."),
+    stringr::str_glue("@title {asgs_geography}"),
+    stringr::str_glue(
+      "@description Fetch ASGS geometries for the **{asgs_geography}** geography."),
     "@details",
+    asgs_description,
+    "",
+    "@section Availability:",
     "The following combinations of ASGS `edition` and `reference_date` are available:",
-    service_tbl(geography_name),
+    rd_service_tbl(geography_name),
     "",
     "@inheritParams get_geography",
     "@export"
   )
 }
 
-service_tbl <- function(geography_name) {
+#' For a given geography, generate a table of ASGS editions and years for which
+#' the geography is available
+#'
+#' @inheritParams autodoc
+#'
+#' @return A character string containing a markdown table.
+#'
+#' @noRd
+rd_service_tbl <- function(geography_name) {
   services |>
     dplyr::filter(geography == geography_name) |>
     dplyr::select(geography, edition, reference_date) |>
@@ -21,4 +44,37 @@ service_tbl <- function(geography_name) {
                  col.names = c("**`geography`**",
                                "**`edition`**",
                                "**`reference_date`**"))
+}
+
+#' Use metadata from the most recent release of a geography.
+#'
+#' Early editions of the ASGS web service do not consistently have content in
+#' the their metadata fields. For example, ASGS1 does not provide any content
+#' via the 'Description' field in its ArcGIS MapServers.
+#'
+#' @inheritParams autodoc
+#' @param field The field in `services` from which to retrieve content.
+#'
+#' @return A character string containing the content from `services[field]` for
+#'   the most recent `reference_date` for the given `geography_name`.
+#'
+#' @noRd
+latest_metadata <- function(geography_name, field) {
+  services |>
+    dplyr::filter(geography == geography_name) |>
+    dplyr::arrange(desc(reference_date)) |>
+    dplyr::slice_head(n = 1) |>
+    dplyr::pull({{field}})
+}
+
+clean_description <- function(description) {
+  description |>
+    stringr::str_replace_all(
+      pattern = c(
+        r"(<DIV STYLE="text-align:Left;font-size:12pt"><P><SPAN>The Australian Statistical Geography Standard \(ASGS\) provides users with an integrated set of standard regions that they can use to analyse and integrate statistics produced by the ABS and other organisations. )" = "",
+        "</SPAN></P></DIV>" = "",
+        r"(Australian Statistical Geography Standard \(ASGS\) Edition 3\nhttps://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026)"  =
+          "[Australian Statistical Geography Standard (ASGS) Edition 3](https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026)",
+        "\n" = "\n\n")) |>
+    stringr::str_split_1("\n")
 }
--- R/getters.R
+++ R/getters.R
@@ -1,77 +1,77 @@
-#' @eval autodoc("ADD")
+#' @eval rd_autodoc("ADD")
 add <- new_getter("ADD")
 
-#' @eval autodoc("AUS")
+#' @eval rd_autodoc("AUS")
 aus <- new_getter("AUS")
 
-#' @eval autodoc("CED")
+#' @eval rd_autodoc("CED")
 ced <- new_getter("CED")
 
-#' @eval autodoc("DZN")
+#' @eval rd_autodoc("DZN")
 dzn <- new_getter("DZN")
 
-#' @eval autodoc("GCCSA")
+#' @eval rd_autodoc("GCCSA")
 gccsa <- new_getter("GCCSA")
 
-#' @eval autodoc("IARE")
+#' @eval rd_autodoc("IARE")
 iare <- new_getter("IARE")
 
-#' @eval autodoc("ILOC")
+#' @eval rd_autodoc("ILOC")
 iloc <- new_getter("ILOC")
 
-#' @eval autodoc("IREG")
+#' @eval rd_autodoc("IREG")
 ireg <- new_getter("IREG")
 
-#' @eval autodoc("LGA")
+#' @eval rd_autodoc("LGA")
 lga <- new_getter("LGA")
 
-#' @eval autodoc("MB")
+#' @eval rd_autodoc("MB")
 mb <- new_getter("MB")
 
-#' @eval autodoc("NRMR")
+#' @eval rd_autodoc("NRMR")
 nrmr <- new_getter("NRMR")
 
-#' @eval autodoc("POA")
+#' @eval rd_autodoc("POA")
 poa <- new_getter("POA")
 
-#' @eval autodoc("RA")
+#' @eval rd_autodoc("RA")
 ra <- new_getter("RA")
 
-#' @eval autodoc("SA1")
+#' @eval rd_autodoc("SA1")
 sa1 <- new_getter("SA1")
 
-#' @eval autodoc("SA2")
+#' @eval rd_autodoc("SA2")
 sa2 <- new_getter("SA2")
 
-#' @eval autodoc("SA3")
+#' @eval rd_autodoc("SA3")
 sa3 <- new_getter("SA3")
 
-#' @eval autodoc("SA4")
+#' @eval rd_autodoc("SA4")
 sa4 <- new_getter("SA4")
 
-#' @eval autodoc("SAL")
+#' @eval rd_autodoc("SAL")
 sal <- new_getter("SAL")
 
-#' @eval autodoc("SED")
+#' @eval rd_autodoc("SED")
 sed <- new_getter("SED")
 
-#' @eval autodoc("SOS")
+#' @eval rd_autodoc("SOS")
 sos <- new_getter("SOS")
 
-#' @eval autodoc("SOSR")
+#' @eval rd_autodoc("SOSR")
 sosr <- new_getter("SOSR")
 
-#' @eval autodoc("SSC")
+#' @eval rd_autodoc("SSC")
 ssc <- new_getter("SSC")
 
-#' @eval autodoc("STE")
+#' @eval rd_autodoc("STE")
 ste <- new_getter("STE")
 
-#' @eval autodoc("SUA")
+#' @eval rd_autodoc("SUA")
 sua <- new_getter("SUA")
 
-#' @eval autodoc("TR")
+#' @eval rd_autodoc("TR")
 tr <- new_getter("TR")
 
-#' @eval autodoc("UCL")
+#' @eval rd_autodoc("UCL")
 ucl <- new_getter("UCL")
