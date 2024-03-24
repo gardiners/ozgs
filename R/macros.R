353a4224300df0e80895bc929abd7f5b3d25c8fb
Sam Gardiner <7697866+gardiners@users.noreply.github.com>
2024-03-24 17:57:49 +1100

    Rename autodoc() -> rd_autodoc()

--- R/macros.R
+++ R/macros.R
@@ -1,7 +1,7 @@
 gen_getter_code <- function(geography_name) {
   cat(
     stringr::str_glue('
-#\' @eval autodoc("{geography_name}")
+#\' @eval rd_autodoc("{geography_name}")
 {stringr::str_to_lower(geography_name)} <- new_getter("{geography_name}")\n\n'
     )
   )
